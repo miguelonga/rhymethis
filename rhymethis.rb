88055246668b7640b670976cfeb445a5d3f52ee4
Miguel Mundi <miguelmundiaragones@gmail.com>
2015-04-15 19:27:12 +0200

    div word

--- rhymethis.rb
+++ rhymethis.rb
@@ -3,11 +3,18 @@ require 'sinatra'
 wordarray = ["carretera", "coche", "señal", "gafas", "policia", "loco", "avería", "reproche", "mofe"]
 wordarray2 = ["avion", "piloto", "azafata", "pasajeros", "pistola", "cabeza", "avería", "disparo", "muerte"]
 wordarray3 = ["histora", "felicidad", "puntos", "ganar", "matar", "mente", "constante", "pensamiento", "desafio"]
+wordarray5 = ["policia", "carro", "puntos", "ventanilla", "colilla", "fumando", "pensando", "la loquera", "Energia", "Hijo del sol", "the litle Vila", "fucking KALIFA", "maria", "familia",
+]
+wordarray4 = ["tentacion", "opcion ", "situacion", "emocion", "razon", "imaginación", "intuicion", "sillón ", "balcón", "tensión", "seduccion", "pasion", "censura", "cintura", "amargura", "locura", "ternura", "dulzura", "desnuda", "fisura", "sepultura","armadura","atadura","caradura", "luna", "suda", "textura", "escritura" ,"hermosura"]
 
 
 get '/' do
-  @word = wordarray.sample
+
+  @word = wordarray.sample 
+  @counter = 0
+  @counter += 1
   erb :word
+
 end
 
 get '/2' do
@@ -19,6 +26,15 @@ get '/3' do
   @word = wordarray3.sample
   erb :word
 end
+
+get '/4' do
+  @word = wordarray4.sample
+  @word2 = wordarray4.sample
+
+  erb :word
+end
+
+
 
 # post '/make-history' do
 #   myarray = (params[:words].split).cycle.to_enum
